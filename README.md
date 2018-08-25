135c320bf3e85ce49bb1433433c425a7a858168b
Vasudev R Nair <vasudevrnair@49.80.68.116.asianet.co.in>
2018-08-25 17:28:15 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Markandeya.github.io
+
+This is the github repository of my **portfolio website** based on ***Jekyll*** and ***Sass*** :blush:.
+
+> Visit the website [here](https://vasudevnair.me "Vasudev R Nair's portfolio") :point_left:.
---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+// Selecting DOM elements
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+const overlay = document.querySelector('#overlay');
+
+// Hide preloader on page load
+
+window.addEventListener('load', function()
+{
+    overlay.style.display = 'none';
+});
+
+
+// Initial menu state
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
+}
